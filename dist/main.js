8a5bc80e0f9d9ff4bb6e88d47b11435b468f33ed
iusmanof <iusmanof@gmail.com>
2026-05-11 05:27:42 +0300

    feat: create config modules

--- dist/main.js
+++ dist/main.js
@@ -6,10 +6,10 @@ const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const config = new swagger_1.DocumentBuilder()
-        .setTitle('Cats example')
-        .setDescription('The cats API description')
+        .setTitle('Quiz game')
+        .setDescription('The game description')
         .setVersion('1.0')
-        .addTag('cats')
+        .addTag('PairQuizGame')
         .build();
     const documentFactory = () => swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api', app, documentFactory);
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { swaggerConfig } from './core/config/swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+ 
+  swaggerConfig(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+// https://github.com/it-incubator/nestjs/blob/main/examples/nestjs/typeorm-postgresql-wallets-entity-repository/src/main.ts
+
+// import { NestFactory } from '@nestjs/core';
+// import { appSetup } from './setup/app.setup';
+// import { CoreConfig } from '@core/core.config';
+// import { initAppModule } from './init-app-module';
+//
+// async function bootstrap() {
+//     const DynamicAppModule = await initAppModule();
+//     // создаём на основе донастроенного модуля наше приложение
+//     const app = await NestFactory.create(DynamicAppModule);
+//
+//     const coreConfig = app.get<CoreConfig>(CoreConfig);
+//
+//     appSetup(app, coreConfig.isSwaggerEnabled); //глобальные настройки приложения
+//
+//     const port = coreConfig.port;
+//
+//     await app.listen(port, () => {
+//         console.log('App starting listen port: ', port);
+//         console.log('NODE_ENV: ', coreConfig.env);
+//     });
+// }
+// bootstrap();
