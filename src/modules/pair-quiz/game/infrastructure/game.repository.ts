1d029773bdb81f480ec98654ee9dab6bf8f497c3
iusmanof <iusmanof@gmail.com>
2026-07-04 01:14:42 +0300

    fix(game): delete await new Promise((resolve) => setTimeout(resolve, 50));

--- src/modules/pair-quiz/game/infrastructure/game.repository.ts
+++ src/modules/pair-quiz/game/infrastructure/game.repository.ts
@@ -93,7 +93,6 @@ class GameRepository {
   }
 
   async save(game: Game): Promise<Game> {
-    // await new Promise((resolve) => setTimeout(resolve, 50));
     return this.dataSource.getRepository(Game).save(game);
   }
 
